98676afb95f38d98608fa957deb3c8be7df4c534
Bruno Rodrigues <brunorodriguesricardo@gmail.com>
2021-10-06 19:25:35 -0300

    feat: new options in scss formatOptions

--- src/generators/asset-types/scss.ts
+++ src/generators/asset-types/scss.ts
@@ -2,16 +2,43 @@ import { FontGenerator } from '../../types/generator';
 import { FontAssetType } from '../../types/misc';
 import { renderTemplate } from '../../utils/template';
 import { renderSrcAttribute } from '../../utils/css';
+import { TEMPLATES_DIR } from '../../constants';
+import { join, dirname, parse } from 'path';
+import fs from 'fs';
 
 const generator: FontGenerator<Buffer> = {
   dependsOn: FontAssetType.SVG,
 
-  generate: (options, svg: Buffer) =>
-    renderTemplate(
+  generate: (options, svg: Buffer) => {
+    const filename = parse(options.pathOptions.scss).name;
+    let generateSrc: boolean | undefined;
+    if (options?.formatOptions?.scss?.splitFontSrcToOtherFile) {
+      generateSrc = false;
+      renderTemplate(
+        join(TEMPLATES_DIR, 'scss_fontSrc.hbs'),
+        { ...options, fontSrc: renderSrcAttribute(options, svg) },
+        { helpers: { codepoint: str => str.toString(16) } }
+      ).then(response => {
+        const path = join(
+          dirname(options.pathOptions.scss),
+          `${filename}_fontSrc.scss`
+        );
+        fs.writeFileSync(path, response);
+      });
+    }
+    console.log(options.formatOptions.scss.generateClasses)
+    return renderTemplate(
       options.templates.scss,
-      { ...options, fontSrc: renderSrcAttribute(options, svg) },
+      {
+        ...options,
+        generateClasses: options.formatOptions.scss.generateClasses,
+        fontSrc: renderSrcAttribute(options, svg),
+        generateSrc,
+        filename
+      },
       { helpers: { codepoint: str => str.toString(16) } }
-    )
+    );
+  }
 };
 
 export default generator;
